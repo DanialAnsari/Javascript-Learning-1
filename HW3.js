8b1d0fa5bbd1834f944bc2f4409e8ad27043133a
unknown <danialansari998@gmail.com>
2023-09-30 12:49:43 +0500

    class 3

---
+++ HW3.js
@@ -0,0 +1,5 @@
+let num;
+
+
+// #1
+//write a code using loops to generate a fibonacci series 
---
+++ forLoops.js
@@ -0,0 +1,24 @@
+
+let i=3;
+let j =1;
+let value = true
+// while(value){
+//     let sum = i+j
+//     console.log(sum)
+//     if(sum%19==0){
+//         value = false
+//         console.log("found")
+//     }
+//     j++;
+// }
+
+// for(let i=0;i<100;i++){
+//     //     console.log(i);
+//     // }
+do{
+    console.log("Work",i)
+    i--;
+}while(i>1)
+
+// 
+console.log("Exit")
